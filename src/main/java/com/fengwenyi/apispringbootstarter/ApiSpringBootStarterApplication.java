54d6aa8f7fcb025704b6275180f47c2d4379ab8c
冯文议 <xfsy_2015@163.com>
2021-08-09 17:39:21 +0800

    build: init

---
+++ src/main/java/com/fengwenyi/apispringbootstarter/ApiSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.apispringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/apispringbootstarter/ApiSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.apispringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
